137e4ff5493fe37d666900386cafc17be52c975e
NikitaBashtannik <Mnogosochinya20001@gmail.com>
2021-11-02 14:50:23 +0600

    Update V0041__create_timeandprice.sql

--- src/main/resources/db/migration/V0041__create_timeandprice.sql
+++ src/main/resources/db/migration/V0041__create_timeandprice.sql
@@ -1,1 +1,1 @@
-INSERT INTO TIMEANDPRICE SET ID = hibernate_sequence_timeandprice.NEXTVAL, PRICE_PLACE = '500R', TIME_PLACE = '15-00', CAR_ID = 1, PLACE_ID = 1;
+INSERT INTO TIMEANDPRICE SET ID = hibernate_sequence_timeandprice.NEXTVAL, PRICE = '500R', TIME = '15-00', CAR_ID = 1, PLACE_ID = 1;
